b42054110bdd3f8a7496496ff4a3ef8a3ee48f50
houndhollis <houndhollis9797@gmail.com>
2023-04-19 22:41:43 +0900

    fix: EsLint - 변수 에러 해결 명령어

--- linter/src/App.js
+++ linter/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 function App() {
+
+  // eslint-disable-line 
+  // esLint 가 사용하지 않는 변수명을 그냥 에러로 잡아낼 경우 
+  
+  const [count, setCount] = React.useState();
+
   return (
     <div className="App">
       <div>초기세팅</div>
